92fa3f7b2619b65c4bce678a2d526feae7889eae
bobo <dongkaige@gmail.com>
2021-11-19 16:07:34 +0800

    update version support

--- src/chains-config/metadata-consts/dbcConsts.ts
+++ src/chains-config/metadata-consts/dbcConsts.ts
@@ -3,11 +3,11 @@ import { MetadataConsts } from '../../types/chains-config';
 
 export const dbcDefinitions: MetadataConsts[] = [
 	{
-		runtimeVersions: [264],
+		runtimeVersions: [264,265,266,267,268,269,270],
 		extrinsicBaseWeight,
 	},
 	{
-		runtimeVersions: [264],
+		runtimeVersions: [264,265,266,267,268,269,270],
 		perClass,
 	},
 ];
